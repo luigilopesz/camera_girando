9bee661a8b1ed080f549713aab23c164d896b1ae
luigilopesz <luigilopes09@gmail.com>
2024-09-16 18:56:31 -0300

    arrumei o pip

--- build/lib/camera_girando/__init__.py
+++
@@ -1,6 +0,0 @@
-from .getcamera import *
-
-__author__ = 'Luigi Lopes'
-__email__ = 'luigilopes09@gmail.com'
-__version__ = '0.1.0'
-
--- setup.py
+++ setup.py
@@ -1,45 +1,28 @@
 from setuptools import setup, find_packages
 import os
 
-
-def find_subdir(start_dir):
-    # Get the list of all subdirectories starting at the given path
-    subdirectories = [x[0] for x in os.walk(start_dir)]
-    subdirectories = [x.split('/',1)[-1]+'/*' for x in subdirectories]
-    return subdirectories
-
-# Lendo o conteúdo do README.md para usar como descrição longa
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-MODULE_STUB = "camera_girando"
-
 setup(
-    name=MODULE_STUB,  # Substitua pelo nome do seu pacote
-    version="0.1.0",
-    author="Luigi Lopes",
-    author_email="luigilopes09@gmail.com",
-    description="camera girando APS3",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/luigilopesz/camera_girando",  # URL do repositório do seu projeto (se houver)
-    packages=find_packages(),  # Encontra automaticamente todos os pacotes no diretório
-    package_data={
-    '': find_subdir(f'{MODULE_STUB}/assets'),
-    },
-    include_package_data=True,
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.11',
-    entry_points={
-        'console_scripts': [
-            f'camera_girando-cli={MODULE_STUB}.main:main',
-        ],
-    },
-    install_requires=[  # Instala as dependências especificadas no requirements.txt
-        line.strip() for line in open("requirements.txt").readlines()
-    ],
+  name="camera",  # Nome do seu pacote
+  version="0.1",
+  packages=find_packages(),  # Encontrar automaticamente todos os pacotes
+  install_requires=[line.strip() for line in open("requirements.txt").readlines()],  # Adicione dependências aqui, se necessário
+  entry_points={
+      'console_scripts': [
+        'camera_girando=camera_girando.main:main',  # Nome do comando CLI e função a ser chamada
+      ],
+  },
+
+  # Metadados para o PyPI (Python Package Index)
+  author="Luigi Carmona de Miranda Lopes",
+  author_email="luigilopes09@gmail.com",
+  description="camera do Luigi!",
+  long_description=open("README.md").read() if os.path.exists("README.md") else "",
+  long_description_content_type="text/markdown",
+  url="https://github.com/luigilopesz/camera_girando",  # Substitua pelo URL do seu repositório, se houver
+  classifiers=[
+    "Programming Language :: Python :: 3",
+      "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ],
+  python_requires='>=3.6',  # Requer Python 3.6 ou superior
 )
